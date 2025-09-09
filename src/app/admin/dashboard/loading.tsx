1d7ac57ffe6e27694ddd18bc08bc2c5085f5c35a
Prajil K <prajilk20017@gmail.com>
2025-09-09 16:20:34 +0530

    added loading

---
+++ src/app/admin/dashboard/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div className="w-full py-20 text-center">
+            <h1>Loading...</h1>
+        </div>
+    );
+};
+
+export default Loading;
