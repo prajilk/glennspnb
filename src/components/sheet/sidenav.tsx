386b8ca00ab2023779d8daf90de855594190c68e
Prajil K <prajilk20017@gmail.com>
2025-09-17 12:41:07 +0530

    fix sidenav scroll issue

--- src/components/sheet/sidenav.tsx
+++ src/components/sheet/sidenav.tsx
@@ -9,7 +9,6 @@ import {
     SheetTitle,
     SheetTrigger,
 } from "../ui/sheet";
-import Link from "next/link";
 import type { HomePageDocument } from "@/models/types/home-page";
 import { Button } from "../ui/button";
 import { useState } from "react";
@@ -18,7 +17,16 @@ type HeaderProps = Pick<HomePageDocument["header"], "headerMenu">;
 
 const Sidenav = ({ headerMenu }: HeaderProps) => {
     const [isOpen, setIsOpen] = useState(false);
+
+    function scrollTo(element: string) {
+        setIsOpen(false); // close the sheet first
+        setTimeout(() => {
+            const section = document.querySelector(element);
+            section?.scrollIntoView({ behavior: "smooth", block: "start" });
+        }, 300); // wait for sheet close animation (~300ms)
+    }
     if (!headerMenu || headerMenu.length === 0) return null;
+
     return (
         <Sheet open={isOpen} onOpenChange={setIsOpen}>
             <SheetTrigger className="md:hidden">
@@ -34,23 +42,20 @@ const Sidenav = ({ headerMenu }: HeaderProps) => {
                 <ul className="px-5 space-y-5">
                     {headerMenu.slice(0, 3).map((item) => (
                         <li key={item.name}>
-                            <Link
-                                href={item.url}
-                                onClick={() => setIsOpen(false)}
-                            >
+                            <button onClick={() => scrollTo(item.url)}>
                                 {item.name}
-                            </Link>
+                            </button>
                         </li>
                     ))}
                     <li>
-                        <Link
-                            href={headerMenu.at(-1)?.url || "#contact"}
-                            onClick={() => setIsOpen(false)}
+                        <Button
+                            className="rounded-full uppercase tracking-wider px-6 pt-2.5 pb-2 h-fit"
+                            onClick={() =>
+                                scrollTo(headerMenu.at(-1)?.url || "#contact")
+                            }
                         >
-                            <Button className="rounded-full uppercase tracking-wider px-6 pt-2.5 pb-2 h-fit">
-                                {headerMenu.at(-1)?.name || "Contact Us"}
-                            </Button>
-                        </Link>
+                            {headerMenu.at(-1)?.name || "Contact Us"}
+                        </Button>
                     </li>
                 </ul>
             </SheetContent>
